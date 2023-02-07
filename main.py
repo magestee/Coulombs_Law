3c26aeec4e6bbafa28aaf08d66c59ea3a3a8b0e7
Saman Gohari <mage@Samans-MacBook-Air.local>
2023-02-07 16:03:47 +0330

    :memo: added the todo list

---
+++ main.py
@@ -0,0 +1,9 @@
+## electric charge is q = ne where n is an integer.
+""" and e = -1.6 * 10**-19 c """
+## matter with alike charges repell each other and matter with different charges attract eachother.
+## so how can we predict how strong the foces between them are going to be?
+## coulumb law is:
+""" f = k*q1*q2 / r**2 """
+""" ke = 8.98 * 10**9 """
+
+#### FIRST: WE NEED A CALCULATOR FOR COILOMBS LAW ####
